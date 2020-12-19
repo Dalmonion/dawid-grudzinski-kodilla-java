72c0e589c2f046ebeb7d56dd5661a9c96e2a8d03
Dawid Grudziński <ddgrudzinski@gmail.com>
2020-12-19 14:42:31 +0100

    7.1 Introduction to functional programming

---
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -0,0 +1,38 @@
+package com.kodilla.stream;
+
+import com.kodilla.stream.beautifier.PoemBeautifier;
+import com.kodilla.stream.lambda.Executor;
+import com.kodilla.stream.lambda.ExpressionExecutor;
+import com.kodilla.stream.lambda.Processor;
+import com.kodilla.stream.reference.FunctionalCalculator;
+
+import java.util.Locale;
+
+public class StreamMain {
+
+    public static void main(String[] args) {
+//        Processor processor = new Processor();
+//        Executor codeToExecute = () -> System.out.println("This is an example text.");
+//        processor.execute(codeToExecute);
+//
+//        ExpressionExecutor expressionExecutor = new ExpressionExecutor();
+//
+//        System.out.println("Calculating expressions with lambdas");
+//        expressionExecutor.executeExpression(10, 5, (a, b) -> a + b);
+//        expressionExecutor.executeExpression(10, 5, (a, b) -> a - b);
+//        expressionExecutor.executeExpression(10, 5, (a, b) -> a * b);
+//        expressionExecutor.executeExpression(10, 5, (a, b) -> a / b);
+//
+//        System.out.println("Calculating expressions with method references");
+//        expressionExecutor.executeExpression(3, 4, FunctionalCalculator::multiplyAByB);
+//        expressionExecutor.executeExpression(3, 4, FunctionalCalculator::addAToB);
+//        expressionExecutor.executeExpression(3, 4, FunctionalCalculator::subBFromA);
+//        expressionExecutor.executeExpression(3, 4, FunctionalCalculator::divideAByB);
+
+        PoemBeautifier poemBeautifier = new PoemBeautifier();
+        poemBeautifier.beautify("This is an example text", n -> n.toUpperCase());
+        poemBeautifier.beautify("This is an example text", n -> "ABC " + n + " ABC");
+        poemBeautifier.beautify("This is an example text", n -> n.repeat(2));
+        poemBeautifier.beautify("This is an example text", n -> n.replace("a", "A"));
+    }
+}
