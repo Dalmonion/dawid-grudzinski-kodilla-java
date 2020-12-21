e1f4991eb784877bc1056060dd60a8f6576b5585
Dawid Grudziński <ddgrudzinski@gmail.com>
2020-12-21 19:34:36 +0100

    7.3 Getting to know stream(), fulter(), map() and collect() functions update

--- kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -7,6 +7,8 @@ import com.kodilla.stream.forumuser.ForumUser;
 import com.kodilla.stream.iterate.NumbersGenerator;
 import com.kodilla.stream.person.People;
 
+import java.time.LocalDate;
+import java.time.Period;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
@@ -97,7 +99,7 @@ public class StreamMain {
         Forum theForum = new Forum();
         Map<Integer, ForumUser> theResultMapOfForumUsers = theForum.getUsersList().stream()
                 .filter(user -> user.getSex() == 'M')
-                .filter(user -> user.getDateOfBirth().getYear() < 2001)
+                .filter(user -> (Period.between(user.getDateOfBirth(), LocalDate.now())).getYears() >= 20)
                 .filter(user -> user.getPublishedPostAmount() > 0)
                 .collect(Collectors.toMap(ForumUser::getUserId, ForumUser -> ForumUser));
 
