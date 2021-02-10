79053ee6d0809fdaff7bab22d7d78114ab0bdbac
Dawid Grudziński <ddgrudzinski@gmail.com>
2021-02-10 17:44:26 +0100

    14 update

--- kodilla-spring/src/main/java/com/kodilla/spring/portfolio/BoardConfig.java
+++ kodilla-spring/src/main/java/com/kodilla/spring/portfolio/BoardConfig.java
@@ -4,39 +4,22 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
 
 @Configuration
 public class BoardConfig {
 
     @Autowired
-    @Qualifier("taskList1")
-    TaskList taskList1;
-
-    @Autowired
-    @Qualifier("taskList2")
-    TaskList taskList2;
-
-    @Autowired
-    @Qualifier("taskList3")
-    TaskList taskList3;
+    TaskList taskList;
 
     @Bean
     public Board getBoard() {
-        return new Board(taskList1, taskList2, taskList3);
+        return new Board(taskList, taskList, taskList);
     }
 
-    @Bean(name = "taskList1")
-    public TaskList getToDoList() {
-        return new TaskList();
-    }
-
-    @Bean(name = "taskList2")
-    public TaskList getInProgressList() {
-        return new TaskList();
-    }
-
-    @Bean(name = "taskList3")
-    public TaskList getDoneList() {
+    @Bean
+    @Scope("prototype")
+    public TaskList getTaskList() {
         return new TaskList();
     }
 
